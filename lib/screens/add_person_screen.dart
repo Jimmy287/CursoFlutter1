cb4bad235a04c64d78239607f885dfc126cba159
Paul Realpe <paulmrg461@gmail.com>
2022-03-08 15:47:22 -0500

    Appbar modificado

--- lib/screens/add_person_screen.dart
+++ lib/screens/add_person_screen.dart
@@ -4,7 +4,7 @@ class AddPersonPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Personas')),
+      appBar: AppBar(title: Text('Personas Appbar')),
       body: Center(
         child: Text('Add persons'),
       ),
